3b6e9a81c31a6ae09631e72837c527ef386bdcbb
irshad ahmad sheikh <ishaikh@luxoft.com>
2021-09-22 00:06:52 +0530

    Custom configuration. - jwt decoder - jwt custom converter - custom authorization

--- src/main/java/com/initgrep/apps/nauth/config/SecurityConfig.java
+++ src/main/java/com/initgrep/apps/nauth/config/SecurityConfig.java
@@ -2,31 +2,60 @@ package com.initgrep.apps.nauth.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter{
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Value("${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}")
     private String jwkSetUri;
 
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeRequests().anyRequest().authenticated()
+//                .and()
+//                .oauth2ResourceServer()
+//                    .jwt().jwkSetUri(jwkSetUri);
+//    }
+
     @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeRequests().anyRequest().authenticated()
+//                .and()
+//                .oauth2ResourceServer()
+//                    .jwt().decoder(NimbusJwtDecoder.withJwkSetUri(jwkSetUri).build());
+//    }
+
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests().anyRequest().authenticated()
                 .and()
                 .oauth2ResourceServer()
-                    .jwt().jwtAuthenticationConverter(new JwtAuthenticationConverter());
+                .jwt().jwtAuthenticationConverter(jwtAuthenticationConverter());
     }
-
 
     @Bean
-    public JwtDecoder jwtDecoder(){
-        return NimbusJwtDecoder.withJwkSetUri(jwkSetUri).build();
+    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtGrantedAuthoritiesConverter grantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+
+        //change the prefix
+        grantedAuthoritiesConverter.setAuthorityPrefix("ROLE_");
+
+        //change the default claim name. default claim is "scope", "scp"
+        grantedAuthoritiesConverter.setAuthoritiesClaimName("name");
+
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(grantedAuthoritiesConverter);
+        return jwtAuthenticationConverter;
     }
+
 }
--- src/main/java/com/initgrep/apps/nauth/user/UserController.java
+++ src/main/java/com/initgrep/apps/nauth/user/UserController.java
@@ -1,5 +1,7 @@
 package com.initgrep.apps.nauth.user;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +15,8 @@ public class UserController {
 
     @GetMapping("/users")
     public List<User> getUsers(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        System.out.println("Authentication - "+authentication);
         return Arrays.asList(
                 new User("irshad", 100),
                 new User("sheikh",300)
