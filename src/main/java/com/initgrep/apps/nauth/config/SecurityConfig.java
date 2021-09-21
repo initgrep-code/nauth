cbecbf23ca726008fc7bdbcb792f7c8c7d22fad0
irshad ahmad sheikh <ishaikh@luxoft.com>
2021-09-21 23:09:50 +0530

    Custom configuration. - jwt decoder - jwt custom converter

--- src/main/java/com/initgrep/apps/nauth/config/SecurityConfig.java
+++ src/main/java/com/initgrep/apps/nauth/config/SecurityConfig.java
@@ -1,7 +1,32 @@
 package com.initgrep.apps.nauth.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
+    @Value("${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}")
+    private String jwkSetUri;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests().anyRequest().authenticated()
+                .and()
+                .oauth2ResourceServer()
+                    .jwt().jwtAuthenticationConverter(new JwtAuthenticationConverter());
+    }
+
+
+    @Bean
+    public JwtDecoder jwtDecoder(){
+        return NimbusJwtDecoder.withJwkSetUri(jwkSetUri).build();
+    }
 }
