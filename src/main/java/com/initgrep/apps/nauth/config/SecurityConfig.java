bc41a5e84d83edd315239cb77048d4c0113f5c1b
irshad ahmad sheikh <ishaikh@luxoft.com>
2021-09-22 23:59:55 +0530

    Custom configuration. - jwt decoder CLock skew - validation of timestamps - configuration of timeouts

--- src/main/java/com/initgrep/apps/nauth/config/SecurityConfig.java
+++ src/main/java/com/initgrep/apps/nauth/config/SecurityConfig.java
@@ -1,14 +1,21 @@
 package com.initgrep.apps.nauth.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
+import org.springframework.security.oauth2.core.OAuth2TokenValidator;
+import org.springframework.security.oauth2.jwt.*;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.web.client.RestOperations;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.Duration;
 
 @EnableWebSecurity
 @Configuration
@@ -56,6 +63,26 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
         jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(grantedAuthoritiesConverter);
         return jwtAuthenticationConverter;
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder(RestTemplateBuilder restTemplateBuilder) {
+
+        RestTemplate restOperations = restTemplateBuilder
+                .setConnectTimeout(Duration.ofSeconds(90))
+                .setReadTimeout(Duration.ofSeconds(90))
+                .build();
+
+        NimbusJwtDecoder nimbusJwtDecoder = NimbusJwtDecoder
+                .withJwkSetUri(jwkSetUri)
+                .restOperations(restOperations)
+                .build();
+
+        OAuth2TokenValidator<Jwt> clockSkew
+                = new DelegatingOAuth2TokenValidator<>(new JwtTimestampValidator(Duration.ofSeconds(60)));
+
+        nimbusJwtDecoder.setJwtValidator(clockSkew);
+        return nimbusJwtDecoder;
     }
 
 }
